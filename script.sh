af5331a41636510a58ff43bedfd8292266c7b219
Ablfxl004 <abolfazl.taghvi004@gmail.com>
2025-08-03 19:18:31 +0330

    convert to FOP

--- script.sh
+++ script.sh
@@ -1,68 +1,100 @@
 #!/bin/bash
 
-pwd=$(pwd)
-script_file_name="script.sh"
-echo The current working directory is $pwd
 
-shopt -s nullglob 
-
-declare -a files_array=()
-
-for entry in *; do
-    if [[ -f "$entry" ]] && [[ "$entry" != "$script_file_name" ]] && [[ "$entry" != "README.md" ]] && [[ "$entry" != "LICENSE.md" ]]; then
-        files_array+=("$entry")
-    fi
-done
-
-
-echo "Files in the current directory:"
-for file in "${files_array[@]}"; do
-    echo "$file"
-done
+files_array=()
 
 file_formats_array=()
 
-echo "File formats in the current directory:"
-for file in "${files_array[@]}"; do
-    format="${file##*.}"
-    if ! [[ "${file_formats_array[*]}" =~ "$format" ]]; then
-  	file_formats_array+=("$format")
-	echo "$format"
-    fi
-done
-
-
+shopt -s nullglob 
 folders_array=(*/)
-
-echo "Folder names in the currect directory:"
-for folder in "${folders_array[@]}"; do
-    echo "$folder"
-done
-
 shopt -u nullglob
 
-for format in "${file_formats_array[@]}"; do
-    upper_format="${format^^}"
-    format_folder="${upper_format}_Folder"
-    
-    mkdir -p "$format_folder"
-    if ! [[ "${folders_array[*]}" =~ "$format_folder" ]]; then
-	echo "--- $format_folder created ---"
-	folders_array+=("$format_folder")
-    fi
+show_current_dir () {
+    pwd=$(pwd)
+    echo The current working directory is $pwd
+}
 
-done
+create_files_array () {
+    script_file_name="script.sh"
 
+    shopt -s nullglob 
 
-for format in "${file_formats_array[@]}"; do
-    for file in "${files_array[@]}"; do
-        if [[ "${file##*.}" == "$format" ]]; then
-            upper_format="${format^^}"
-            format_folder="${upper_format}_Folder"
-
-            mv -i "$file" "$format_folder"
+    for entry in *; do
+        if [[ -f "$entry" ]] && [[ "$entry" != "$script_file_name" ]] && [[ "$entry" != "README.md" ]] && [[ "$entry" != "LICENSE.md" ]]; then
+            files_array+=("$entry")
         fi
     done
+    
+    shopt -u nullglob 
+}
 
-done
 
+show_files_array () {
+    echo "Files in the current directory:"
+    for file in "${files_array[@]}"; do
+        echo "$file"
+    done
+}
+
+
+create_file_formats_array() {
+    for file in "${files_array[@]}"; do
+        format="${file##*.}"
+        if ! [[ "${file_formats_array[*]}" =~ "$format" ]]; then
+        file_formats_array+=("$format")
+        fi
+    done
+}
+
+show_file_formats_array(){
+    echo "File formats in the current directory:"
+    for format in "${file_formats_array[@]}"; do
+        echo "$format"
+    done
+}
+
+
+show_exicted_folder() {
+    echo "Folder names in the currect directory:"
+    for folder in "${folders_array[@]}"; do
+        echo "$folder"
+    done
+}
+
+
+create_needed_format_folders() {
+    for format in "${file_formats_array[@]}"; do
+        upper_format="${format^^}"
+        format_folder="${upper_format}_Folder"
+        
+        mkdir -p "$format_folder"
+        if ! [[ "${folders_array[*]}" =~ "$format_folder" ]]; then
+        echo "--- $format_folder created ---"
+        folders_array+=("$format_folder")
+        fi
+
+    done
+}
+
+move_files_to_realated_folder() {
+    for format in "${file_formats_array[@]}"; do
+        for file in "${files_array[@]}"; do
+            if [[ "${file##*.}" == "$format" ]]; then
+                upper_format="${format^^}"
+                format_folder="${upper_format}_Folder"
+                mv -i "$file" "$format_folder"
+            fi
+        done
+
+    done
+}
+
+
+show_current_dir
+create_files_array
+show_files_array
+create_file_formats_array
+show_file_formats_array
+show_exicted_folder
+create_needed_format_folders
+move_files_to_realated_folder
